05e31fdf4f810dff3ee2e4faac2d78e47e4a2164
Justin Carter <jcarter82@gmail.com>
2022-12-14 14:04:51 -0600

    start to add database connection

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,14 @@
 'use strict';
+const mongoose = require('mongoose');
+const user = process.env.user;
+const password = process.env.password;
+const server = process.env.server;
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(`mongodb+srv://${user}:${password}@${server}`, { useNewUrlParser: true, useUnifiedTopology: true})
+	.then( () => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
 
 module.exports = function (app) {
 
